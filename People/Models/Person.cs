db13d317c2071341ec57a18612d3c3e7f6f03741
JohaoG10 <johao.gavilanes@udla.edu.ec>
2025-01-07 22:29:36 -0500

    Agregar archivos de proyecto.

---
+++ People/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace People.Models;
+
+[Table("people")]
+public class Person
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [MaxLength(250), Unique]
+    public string Name { get; set; }
+}
